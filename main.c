96bed471946c07323d0b4aa848b05a423235b828
Sam <sam.peller@comcast.net>
2025-06-04 10:19:35 -0400

    initial commit

--- main.c
+++ main.c
@@ -14,8 +14,27 @@ void boardPrint(char arr[9]) {
     }
 }
 
+char checkWin(char arr[9]) { // Not finished
+    for (int i = 0; i < 9; i++) {
+        if (arr[i] == 'X') {
+            return 'X';
+        }
+        else if (arr[i] == 'O') {
+            return 'O';
+        }
+        else {
+            return '1';
+            }
+    }
+}
+
+char updateBoard(char arr[9]) {
+    // Replace number with X or O and check for win afterwards
+    return checkWin(arr);
+}
+
 int main(void) {
-    char arr[9] = {'*','*','*','*','*','*','*','*','*'};
+    char arr[9] = {'1','2','3','4','5','6','7','8','9'};
 
     boardPrint(arr);
     return 0;
