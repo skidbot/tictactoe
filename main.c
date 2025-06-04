1c9226660ccaf07b714aac8fb1f01cc2399cc54f
Sam <sam.peller@comcast.net>
2025-06-04 10:24:24 -0400

    initial commit

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 void boardPrint(char arr[9]) {
@@ -26,16 +27,21 @@ char checkWin(char arr[9]) { // Not finished
             return '1';
             }
     }
+    return false;
 }
 
 char updateBoard(char arr[9]) {
     // Replace number with X or O and check for win afterwards
-    return checkWin(arr);
+    if (checkWin(arr) == true) {
+        return 'W';
+    }
+
+    return boardPrint(arr);
 }
 
 int main(void) {
     char arr[9] = {'1','2','3','4','5','6','7','8','9'};
-
-    boardPrint(arr);
+    // While Loop for Input from both players until win
+    updateBoard(arr);
     return 0;
 }
