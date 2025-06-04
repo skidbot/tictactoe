ce2bd577596e094055be82e1d53df6d7e1cee35a
Sam <sam.peller@comcast.net>
2025-06-04 10:11:21 -0400

    initial commit

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+void boardPrint(char arr[9]) {
+    for (int i = 0; i < 9; i++) {
+        printf(" %c ", arr[i]);
+        if ((i + 1) % 3 == 0) {
+            printf("\n");
+            if (i < 8) {
+                printf("---|---|---\n");
+            }
+        } else {
+            printf("|");
+        }
+    }
+}
+
 int main(void) {
-    printf("Hello, World!\n");
+    char arr[9] = {'*','*','*','*','*','*','*','*','*'};
+
+    boardPrint(arr);
     return 0;
 }
